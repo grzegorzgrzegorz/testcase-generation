80518df199a65bb9df8bfe7186525e11c43ede69
grzegorzgrzegorz <grzegorz@passfailerror.com>
2023-02-19 20:16:06 +0100

    Add runner to be able to run class under test

---
+++ src/main/java/some_class/RunMyUtil.java
@@ -0,0 +1,9 @@
+package some_class;
+
+public class RunMyUtil {
+
+    public static void main(String[] args) {
+        String result = MyUtil.makeValidSentence("Someword.");
+        System.out.println(result);
+    }
+}
