34c5ca9354eb2d2eba902481f9a359df48cd38ca
grzegorzgrzegorz <grzegorz@passfailerror.com>
2023-02-25 18:22:56 +0100

    Move json processing and deserialize related methods to Utils class

--- src/main/java/deserializer/TestcaseGenerator.java
+++ src/main/java/deserializer/TestcaseGenerator.java
@@ -1,18 +1,13 @@
 package deserializer;
 
-import com.google.gson.Gson;
 import core.DslGenerator;
 import gherkin.GWT;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import some_class_dsl.When;
+import utils.Utils;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +30,17 @@ public class TestcaseGenerator {
         );
     }
 
+    public JSONArray getTestcases(String fileName) throws URISyntaxException {
+        JSONObject jsonObject = Utils.getParsedJson(fileName);
+        return (JSONArray) ((JSONObject) jsonObject.get("makeValidSentence")).get("testCases");
+    }
+
+    public List<DslGenerator> getVariableCombination(JSONObject jsonObject) throws URISyntaxException {
+        List<DslGenerator> inputCombination = new ArrayList<>();
+        jsonObject.keySet().forEach(keyName -> inputCombination.add(Utils.getDeserializedInstance(keyName, jsonObject)));
+        return inputCombination;
+    }
+
     public void printTestcase(List<DslGenerator> variableCombination) {
         System.out.println("//" + GWT.GIVEN);
         System.out.println("DataGenerator dataGenerator = new DataGenerator();");
@@ -45,49 +51,5 @@ public class TestcaseGenerator {
 
         System.out.println("//" + GWT.THEN);
         variableCombination.forEach(item -> item.generateDsl(GWT.THEN));
-    }
-
-    public JSONArray getTestcases(String fileName) throws URISyntaxException {
-        JSONObject jsonObject = getParsedJson(fileName);
-        return (JSONArray) ((JSONObject) jsonObject.get("makeValidSentence")).get("testCases");
-    }
-
-    public List<DslGenerator> getVariableCombination(JSONObject jsonObject) throws URISyntaxException {
-        List<DslGenerator> inputCombination = new ArrayList<>();
-        jsonObject.keySet().forEach(keyName -> inputCombination.add(getSerializedInstance(keyName, jsonObject)));
-        return inputCombination;
-    }
-
-    private JSONObject getParsedJson(String fileName) {
-        Path path = null;
-        try {
-            path = Paths.get(this.getClass().getClassLoader().getResource(fileName).toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        String json = null;
-        try {
-            json = Files.readString(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return new JSONObject(json);
-    }
-
-    public DslGenerator getSerializedInstance(String varName, JSONObject jsonObject) {
-        Class className = getClassFromVarName(varName);
-        String serializedClass = jsonObject.get(varName).toString();
-        return new Gson().fromJson(serializedClass, (Type) className);
-    }
-
-    private Class getClassFromVarName(String varName) {
-        try {
-            if (varName.contains(".")) {
-                varName = varName.split("\\.")[1];
-            }
-            return Class.forName("some_class_dsl." + varName);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,48 @@
+package utils;
+
+import com.google.gson.Gson;
+import core.DslGenerator;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Utils {
+
+    public static JSONObject getParsedJson(String fileName) {
+        Path path = null;
+        try {
+            path = Paths.get(Utils.class.getClassLoader().getResource(fileName).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        String json = null;
+        try {
+            json = Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new JSONObject(json);
+    }
+
+    public static DslGenerator getDeserializedInstance(String varName, JSONObject jsonObject) {
+        Class cls = getClassFromVarName(varName);
+        String className = jsonObject.get(varName).toString();
+        return new Gson().fromJson(className, (Type) cls);
+    }
+
+    private static Class getClassFromVarName(String varName) {
+        try {
+            if (varName.contains(".")) {
+                varName = varName.split("\\.")[1];
+            }
+            return Class.forName("some_class_dsl." + varName);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
